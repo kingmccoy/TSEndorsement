0cd9c5837a94ba64c097af688b860f1571ee79ed
Christian Mark Tibong <eaglemaya30.cmt@gmail.com>
2024-05-30 09:36:40 +0800

    change pc

--- DataBaseConnection.vb
+++ DataBaseConnection.vb
@@ -1,7 +1,7 @@
 ﻿Imports System.Data.SqlClient
 
 Module DataBaseConnection
-    Public dbConn As New SqlConnection("Data Source=10.10.15.25,1444;Initial Catalog=sli_endorsement;Persist Security Info=True;User ID=test;Password=test123;Encrypt=False;Connection Timeout=30;")
+    Public dbConn As New SqlConnection("Data Source=10.10.15.11,1444;Initial Catalog=sli_endorsement;Persist Security Info=True;User ID=test;Password=test123;Encrypt=False;Connection Timeout=30;")
 
     Public Sub Load_Model_Variant()
         Try
